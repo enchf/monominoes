ded0784aea0b154507fbff731cc9aae85215fe6e
enchf <e.ernesto.espinosa@gmail.com>
2015-09-16 13:53:39 -0500

    TagRender first draft

--- src/monominoes.ref.js
+++ src/monominoes.ref.js
@@ -77,17 +77,9 @@ Monominoes.tags.create = function(tag,simple) {
   t.prototype.simple = simple;
   t.prototype.template = (simple === true) ? Monominoes.tags.open : Monominoes.tags.template;
   t.prototype.tag = Monominoes.util.format(t.prototype.template,tag);
-  t.prototype.object = $(t.prototype.tag);
+  t.prototype.build = function() { return $(t.prototype.tag); };
   t.prototype.classtype = t;
   t.prototype.render = Monominoes.tags.render(obj);
-  return obj;
-};
-Monominoes.tags.render = function() {
-  //TODO fix.
-  var obj = $(Monominoes.util.format(this.getTemplate(),this.tag));
-  if (cfg && cfg.class) obj.addClass(cfg.class);
-  if (cfg && cfg.content) obj.html(cfg.content);
-  if (cfg && cfg.parent) obj.appendTo(cfg.parent);
   return obj;
 };
 
@@ -98,9 +90,10 @@ Monominoes.tags.render = function() {
 })();
 
 /** Renders **/
-Monominoes.renders.isRender = function(obj) {
+Monominoes.renders.isRender = function(obj,clazz) {
   var classtype = obj.class;
   var render = false;
+  clazz = (clazz || Monominoes.Render);
   
   while (!render && classtype) {
     render = classtype === Monominoes.Render;
@@ -109,13 +102,19 @@ Monominoes.renders.isRender = function(obj) {
   
   return render;
 };
-Monominoes.renders.create(obj,clazz,cfg) {
-  var t = Monominoes.renders.isRender(obj) ? obj : new clazz();
+Monominoes.renders.create = function(obj,clazz,cfg) {
+  var t = Monominoes.renders.isRender(obj,clazz) ? obj : new clazz();
   t.super = Monominoes.util.apply(t,{});
   Monominoes.util.apply(cfg,t);
   return t;
 };
-Monominoes.renders.extend = function(renderClass,newClass) {};
+Monominoes.renders.extend = function(clazz,superclass,cfg) {
+  Monominoes.util.apply(superclass.prototype,clazz.prototype);
+  Monominoes.util.apply(cfg,clazz.prototype);
+  clazz.prototype.superclass = superclass;
+  clazz.prototype.class = clazz;
+  return clazz;
+};
               
 /** Base abstract Render class **/
 Monominoes.Render = function(cfg) { return Monominoes.renders.create(this,Monominoes.Render,cfg); };
@@ -126,4 +125,16 @@ Monominoes.Render.prototype.formatChild = Monominoes.util.self;
 Monominoes.Render.prototype.attrs = {};
 Monominoes.Render.classtype = Monominoes.Render;
 
-Monominoes.TagRender = Monominoes.renders.extend(Monominoes.Render);
+Monominoes.TagRender = function(cfg) { return Monominoes.renders.create(this,Monominoes.TagRender,cfg); };
+(function() {
+  Monominoes.renders.extend(Monominoes.TagRender,Monominoes.Render,{
+    render: function(item,parent) {
+      var tag = this.tag.build()
+        .html(this.formatChild(item));
+        .addClass(this.class);
+      
+      if (parent) tag.appendTo(parent);
+      return tag;
+    }
+  });
+})();
