573e08559de12ed841c327d550b0935f6a8475fa
enchf <e.ernesto.espinosa@gmail.com>
2015-10-03 17:17:51 -0500

    Migrating general utils to Komunalne.js

--- src/monominoes.util.js
+++ src/monominoes.util.js
@@ -10,37 +10,3 @@
 Monominoes.util.format = function() { 
   return $.validator.format.apply(null,arguments);
 };
-
-/**
- * Generates a new string object from concatenating 'app' (appended) string (if present)
- * to 'str' string (if present), being separated by 'sep' (separator) string (if present, otherwise ' ').
- * The result is trimmed.
- * @param str First place string, optional, default = ''.
- * @param app Second place string, optional, default = ''.
- * @param sep Above strings separator, optional, default = ' '.
- */
-Monominoes.util.append = function(str,app,sep) { 
-  return ((str||"") + (sep||" ") + (app||"")).trim();
-};
-
-/**
- * Lookup in obj for the dot-separated path element.
- * If at some point the current reference is null, the result is null.
- * Pass an empty string as 'path' argument to return 'obj' itself.
- * @param obj The object to lookup.
- * @param path Dot-separated path reference ("a.b.c...")
- */
-Monominoes.util.path = function(obj,path) {
-  if (path === "") return obj;
-  var paths = path.split(".");
-  var el = obj;
-  for (var p in paths) {
-    if (typeof el === "object" && el[paths[p]] !== undefined) {
-      el = el[paths[p]]; 
-    } else { 
-      el = null;
-      break;
-    }
-  }
-  return el;
-};
--- test/monominoes.test.js
+++ test/monominoes.test.js
@@ -8,32 +8,3 @@ QUnit.test("Monominoes Arguments", function(assert) {
   assert.ok(Monominoes.tags,"Monominoes tags container defined");
   assert.ok(Monominoes.renders,"Monominoes renders container defined");
 });
-
-QUnit.test("Append util function", function(assert) {
-  var suite = new Komunalne.test.Suite();
-  
-  suite.add([],"","Call to append without arguments");
-  suite.add([""],"","Call to append with empty string");
-  suite.add(["Str"],"Str","Call to append with single string");
-  suite.add(["A","B"],"A B","Call to append with two strings");
-  suite.add(["A","B","C"],"ACB","Call to append with three strings");
-  suite.add(["A",null],"A","Append null to string");
-  suite.add(["A",null,"C"],"AC","Append null with separator");
-  suite.add([null,"B"],"B","Append string to null");
-  suite.add([null,"B","C"],"CB","Append string to null with separator");
-  
-  Komunalne.test.execute(suite, Monominoes.util.append, new Komunalne.helper.Method(assert.equal,assert));
-});
-
-QUnit.test("Path lookup function", function(assert) {
-  var obj = { "a": 1, "b": [1,2,3], "c": "str", "d": { "e": "e", "f": { "g": false }, "h": true }, "i": -1 };
-  var suite = new Komunalne.test.Suite();
-  
-  suite.add([obj,"a"],1,"Test simple path ['a']");
-  suite.add([obj,"d.e"],"e","Test one depth ['d.e']");
-  suite.add([obj,"d.f.g"],false,"Test two depth ['d.f.g']");
-  suite.add([obj,"d.i.f"],null,"Test unreachable path === null");
-  suite.add([obj,"a.b"],null,"Test try to go deep into a non object");
-  
-  Komunalne.test.execute(suite,Monominoes.util.path,new Komunalne.helper.Method(assert.strictEqual,assert)); 
-});
