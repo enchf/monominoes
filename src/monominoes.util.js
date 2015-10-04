744a7c8e60af1d454579c60f5ea6e8d29c7d37fe
enchf <e.ernesto.espinosa@gmail.com>
2015-10-04 05:03:02 -0500

    Added concrete fn to util

--- src/monominoes.util.js
+++ src/monominoes.util.js
@@ -11,3 +11,13 @@ Monominoes.util.format = function() {
  * @param x Argument to be returned as it is.
  */
 Monominoes.util.self = function(x) { return x; };
+
+/**
+ * Executes the first argument if it is a function, otherwise returns it.
+ * @param fn Object to check whether it is a function.
+ * @param scope Scope to use if the first argument needs to be executed, null otherwise.
+ * @param arguments... Pass any number of arguments after scope if they are required to execute the function.
+ */
+Monominoes.util.concrete = function(fn,scope) { 
+  return K.util.isFunction(fn) ? fn.apply(scope,Array.prototype.slice.call(arguments,2)) : fn; 
+};
--- test/monominoes.test.js
+++ test/monominoes.test.js
@@ -8,3 +8,19 @@ QUnit.test("Monominoes Arguments", function(assert) {
   assert.ok(Monominoes.tags,"Monominoes tags container defined");
   assert.ok(Monominoes.renders,"Monominoes renders container defined");
 });
+
+QUnit.test("Concrete method from util", function(assert) {
+  var suite;
+  
+  suite = new Komunalne.test.Suite();
+  suite.add([function(){ return -1; }],-1,"Function returning -1 passed as argument");
+  suite.add([function(){ return this.a; },{a:1}],1,"Scope passed as argument");
+  suite.add([function(a,b) { return a+b; },null,1,2],3,"Function with arguments");
+  suite.add([function(a,b,c) { return this.d+c+a+b; },{d:4},1,2,3],10,"Function with arguments and scope");
+  suite.add(["string"],"string","Non object passed as argument");
+  suite.add(["string",{a:1},4,5],"string","Other arguments ignored if argument is a concrete object");
+  suite.add(["string",null,4,5],"string","Other arguments ignored if argument is a concrete object with scope as null");
+  
+  suite.execute(Monominoes.util.concrete,assert.buildFor("strictEqual"));
+  assert.strictEqual(Monominoes.util.concrete(),undefined,"Undefined returned if concrete is called with no arguments");
+});
