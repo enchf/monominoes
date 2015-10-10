b15b19dc906f2e3bf903bbebccd986843d725832
enchf <e.ernesto.espinosa@gmail.com>
2015-10-10 02:23:43 -0500

    Tag class test and fixes

--- src/monominoes.tags.js
+++ src/monominoes.tags.js
@@ -1,29 +1,55 @@
-/** Tags **/
+/**
+ * Tag class definition.
+ * @param name Tag name.
+ * @param simple True to build as a self-closing, no-content tag. 
+ *        If no flag specified, it will be determined using Monominoes.Tag.isSimple method.
+ */
 Monominoes.Tag = function(name,simple){
   this.name = name;
-  this.simple = simple === true;
-  this.tag = Monominoes.util.format(this.getTemplate(),name);
-  this.defaultcss = Monominoes.util.format("monominoes monominoes-{0}",name);
+  this.simple = simple === true || Monominoes.Tag.isSimple(name);
+  this.tag = Monominoes.util.format(this.template(),name);
 };
-Monominoes.Tag.prototype.getTemplate = function() { 
-  return this.simple ? Monominoes.Tag.open : Monominoes.Tag.template;
+
+/**
+ * Determines the string template to use depending tag self-closability.
+ */
+Monominoes.Tag.prototype.template = function() { 
+  return this.simple ? Monominoes.Tag.simple : Monominoes.Tag.full;
 };
-Monominoes.Tag.prototype.build = function() { return $(this.tag); };
+
+/**
+ * Build a new HTML DOM jQuery object for the tag, applying the specified configuration.
+ * @param config A configuration object that can contain the following optional properties:
+ * - class: String with the CSS class names to apply to the tag.
+ * - events: Object with key-value pairs of valid events, being key = name as string and value = function handler.
+ * - attrs: Object with key-value string pairs of tag attributes, being key = attribute name and value = attribute value.
+ * - style: Object with key-value pairs of CSS properties, being key = property name and value = CSS valid value.
+ */
+Monominoes.Tag.prototype.build = function(config) { 
+  var tag;
+  config = config || {};
+  tag = $(this.tag).addClass(config.class || "")
+    .attr(config.attrs || {})
+    .css(config.style || {});
+  Komunalne.util.forEach((config.events || {}), function(val,key) { tag.on(key,val); });
+  return tag;
+};
 
 /* Statics */
 Monominoes.Tag.text = ["h1","h2","h3","h4","h5","h6","span","header","strong","i","p","pre","code"];
 Monominoes.Tag.list = ["ul","ol","li"];
 Monominoes.Tag.item = ["div","form","button","a","label"];
 Monominoes.Tag.selfclose = ["img","input","br","hr"];
-Monominoes.Tag.all = Monominoes.Tag.text.concat(Monominoes.Tag.list).concat(Monominoes.Tag.item).concat(Monominoes.Tag.selfclose);
-Monominoes.Tag.open = "<{0}>";
-Monominoes.Tag.close = "</{0}>";
-Monominoes.Tag.template = Monominoes.Tag.open + Monominoes.Tag.close;
-Monominoes.Tag.isSimple = function(tag) { return Monominoes.Tag.selfclose.indexOf(tag) >= 0; };
+Monominoes.Tag.isSimple = function(tag) { return Komunalne.util.arrayContains(tag,this.selfclose); };
+Monominoes.Tag.simple = "<{0}>";
+Monominoes.Tag.full = Monominoes.Tag.simple + "</{0}>";
 
 (function() {
-  var tags = Monominoes.Tag.all;
-  for(var i in tags) {
-    Monominoes.tags[tags[i].toUpperCase()] = new Monominoes.Tag(tags[i],Monominoes.Tag.isSimple(tags[i]));
+  var Tag = Monominoes.Tag;
+  Tag.all = Komunalne.util.arrayConcat(Tag.text,Tag.list,Tag.item,Tag.selfclose);
+  var tag;
+  for(var i in Tag.all) {
+    tag = Tag.all[i];
+    Monominoes.tags[tag.toUpperCase()] = new Monominoes.Tag(tag);
   }
 })();
--- test/monominoes.test.js
+++ test/monominoes.test.js
@@ -2,12 +2,11 @@
  * Monominoes QUnit testing.
  */
 
-QUnit.test("Monominoes Arguments", function(assert) {
+QUnit.test("Monominoes definition", function(assert) {
   assert.ok(Monominoes,"Monominoes object defined");
   assert.ok(Monominoes.util,"Monominoes util container defined");
   assert.ok(Monominoes.tags,"Monominoes tags container defined");
   assert.ok(Monominoes.renders,"Monominoes renders container defined");
-  assert.ok(Monominoes.Tag,"Monominoes Tag class defined");
 });
 
 QUnit.test("Concrete method from util", function(assert) {
@@ -24,4 +23,68 @@ QUnit.test("Concrete method from util", function(assert) {
   
   suite.execute(Monominoes.util.concrete,assert.buildFor("strictEqual"));
   assert.strictEqual(Monominoes.util.concrete(),undefined,"Undefined returned if concrete is called with no arguments");
+});
+
+QUnit.test("Tag class definition", function(assert) {
+  var tag,simple;
+  var custom,customSimple;
+  var Tag = Monominoes.Tag;
+  var f = function(simple) { 
+    return function(tag) { assert.strictEqual(Monominoes.Tag.isSimple(tag),simple,"Self-close definition for " + tag); };
+  };
+  var indicator = 0;
+  var fn = function() { indicator++; };
+  var clasz = "fake";
+  var attrs = { "id": "fake", "title": "name" };
+  var events = { "click": fn, "resize": fn };
+  var style = { "width": "100px", "display": "block" };
+  var emptyCfg = {};
+  var config = { "class": clasz, "attrs": attrs, "events": events, "style": style };
+  var item;
+  
+  assert.ok(Monominoes.Tag,"Monominoes Tag class defined");
+  assert.ok(Monominoes.Tag.all,"All tags array");
+  assert.equal(Monominoes.Tag.all.length,
+               Tag.text.length + Tag.list.length + Tag.item.length + Tag.selfclose.length,
+               "All tags contained in Monominoes.Tag.all");
+  tag = new Monominoes.Tag("h1");
+  simple = new Monominoes.Tag("br");
+  custom = new Monominoes.Tag("foo");
+  customSimple = new Monominoes.Tag("roo",true);
+  
+  assert.notOk(tag.simple,"H1 tag is recognized as a not self-close tag");
+  assert.ok(simple.simple,"BR tag is recognized as a self-close tag");
+  assert.notOk(custom.simple,"Declared not self-close tag");
+  assert.ok(customSimple.simple,"Declared self-close tag");
+  
+  Monominoes.Tag.text.forEach(f(false));
+  Monominoes.Tag.list.forEach(f(false));
+  Monominoes.Tag.item.forEach(f(false));
+  Monominoes.Tag.selfclose.forEach(f(true));
+  
+  item = tag.build(emptyCfg);
+  assert.notOk(item.hasClass(clasz),"Tag creation without class specification");
+  assert.notOk(item.attr("id"),"Tag creation without attributes");
+  assert.notOk(item.prop("id"),"Tag creation without attributes");
+  assert.notOk(item.attr("title"),"Tag creation without attributes");
+  assert.notOk(item.prop("title"),"Tag creation without attributes");
+  assert.equal(item.css("width"),"0px","Tag creation without css properties");
+  assert.notOk(item.css("display"),"Tag creation without css properties");
+  item.trigger("click");
+  assert.equal(indicator,0,"Tag creation without click event");
+  item.trigger("resize");
+  assert.equal(indicator,0,"Tag creation without resize event");
+  
+  item = tag.build(config);
+  assert.ok(item.hasClass(clasz),"Tag creation with class specification");
+  assert.ok(item.attr("id"),"Tag creation with attributes");
+  assert.equal(item.prop("id"),"fake","Tag creation with attributes");
+  assert.ok(item.attr("title"),"Tag creation with attributes");
+  assert.equal(item.prop("title"),"name","Tag creation with attributes");
+  assert.equal(item.css("width"),"100px","Tag creation with css properties");
+  assert.equal(item.css("display"),"block","Tag creation with css properties");
+  item.trigger("click");
+  assert.equal(indicator,1,"Tag creation with click event");
+  item.trigger("resize");
+  assert.equal(indicator,2,"Tag creation with resize event");
 });
