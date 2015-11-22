3b6a602c0c717e72a4d306365e6067b83bc850b9
enchf <e.ernesto.espinosa@gmail.com>
2015-11-22 00:43:13 -0600

    Fixed bug for iterable icons

--- src/monominoes.fa.js
+++ src/monominoes.fa.js
@@ -17,7 +17,7 @@ Monominoes.fa.ICON = Monominoes.renders.I.extend({
   "background": "", // Default existent.
   "buildItem": function(data) {
     var cls = "";
-    var self,item;
+    var self,item,pivot;
     var append = function(target,cls) {
       return Komunalne.util.append(target,Monominoes.util.format("fa-{0}",cls));
     };
@@ -29,17 +29,18 @@ Monominoes.fa.ICON = Monominoes.renders.I.extend({
     
     item = this.super.buildItem(data);
     self = this;
+    pivot = {};
     Komunalne.util.forEach(["size","pull","animated","rotate","flip","icon"],function(val) {
-      self[val] = Monominoes.util.extractValue(self[val],self,data);
+      pivot[val] = Monominoes.util.extractValue(self[val],self,data);
     });
     
     cls = Komunalne.util.append("fa",item.attr("class"));
-    cls = append(cls,this.icon);
-    cls = Komunalne.util.isAnyOf(this.size,"lg","2x","3x","4x","5x") ? append(cls,this.size) : cls;
-    cls = Komunalne.util.isAnyOf(this.pull,"left","right") ? append(cls,"pull-"+this.pull) : cls;
-    cls = Komunalne.util.isAnyOf(this.animated,"pulse","spin") ? append(cls,this.animated) : cls;
-    cls = Komunalne.util.isAnyOf(this.rotate,"90","180","270") ? append(cls,"rotate-"+this.rotate) : cls;
-    cls = Komunalne.util.isAnyOf(this.flip,"horizontal","vertical") ? append(cls,"flip-"+this.flip) : cls;
+    cls = append(cls,pivot.icon);
+    cls = Komunalne.util.isAnyOf(pivot.size,"lg","2x","3x","4x","5x") ? append(cls,pivot.size) : cls;
+    cls = Komunalne.util.isAnyOf(pivot.pull,"left","right") ? append(cls,"pull-"+pivot.pull) : cls;
+    cls = Komunalne.util.isAnyOf(pivot.animated,"pulse","spin") ? append(cls,pivot.animated) : cls;
+    cls = Komunalne.util.isAnyOf(pivot.rotate,"90","180","270") ? append(cls,"rotate-"+pivot.rotate) : cls;
+    cls = Komunalne.util.isAnyOf(pivot.flip,"horizontal","vertical") ? append(cls,"flip-"+pivot.flip) : cls;
     
     cls = this["fixed-width"] === true ? append(cls,"fw") : cls;
     cls = this.border === true ? append(cls,"border") : cls;
--- test/monominoes.test.js
+++ test/monominoes.test.js
@@ -1173,6 +1173,39 @@ QUnit.test("Font Awesome renders", function(assert) {
   assert.ok(item.item.hasClass("fa-camera-retro"),"FA icon classes contains the rendered icon");
   render.clear();
   
-  /*config = {};
-  render = new Monominoes.fa.ICON(config).render(data,"#test-div");*/
+  config = {
+    "iterable": true,
+    "icon": { "path": "icon" },
+    "size": "5x",
+    "fixed-width": true,
+    "border": true,
+    "pull": "left",
+    "animated": function(render,data) { return data.animation; },
+    "flip": "vertical",
+    "rotate": "180",
+    "color": Monominoes.util.property("color"),
+    "background": "black"
+  };
+  data = [
+    { "color": "red", "icon": "cog", "rgb": "rgb(255, 0, 0)", "animation": "spin" },
+    { "color": "white", "icon": "refresh", "rgb": "rgb(255, 255, 255)", "animation": "pulse" }
+  ];
+  render = new Monominoes.fa.ICON(config).render(data,"#test-div");
+  assert.equal(render.items.length,2,"Two elements for FA icon iterable");
+  for (var i = 0; i < 2; i++) {
+    assert.equal(Komunalne.$.elementText((item = render.items[i]).item),"","Text tag is empty");
+    assert.equal(render.layout.children.length,0,"No children renders are set");
+    assert.ok(item.item.hasClass("fa"),"FA icon classes contains fa");
+    assert.ok(item.item.hasClass("fa-"+data[i].icon),"FA icon for item " + i);
+    assert.ok(item.item.hasClass("fa-5x"),"FA icon size set for item " + i);
+    assert.ok(item.item.hasClass("fa-fw"),"FA icon fixed width for item " + i);
+    assert.ok(item.item.hasClass("fa-border"),"FA icon border set for item " + i);
+    assert.ok(item.item.hasClass("fa-pull-left"),"FA icon left alignment for item " + i);
+    assert.ok(item.item.hasClass("fa-"+data[i].animation),"FA icon animation is set for item " + i);
+    assert.ok(item.item.hasClass("fa-flip-vertical"),"FA icon flip set for item " + i);
+    assert.ok(item.item.hasClass("fa-rotate-180"),"FA icon rotation set for item " + i);
+    assert.equal(item.item.css("color"),data[i].rgb,"FA icon color for item " + i);
+    assert.equal(item.item.css("background-color"),"rgb(0, 0, 0)","FA icon background color for item " + i);
+  }
+  render.clear();
 });
