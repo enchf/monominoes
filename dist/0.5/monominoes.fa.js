833e73005bd119aa9caa1996290408639cc46e80
enchf <e.ernesto.espinosa@gmail.com>
2016-01-18 23:51:45 -0600

    Added BS and FA renders to 0.5

---
+++ dist/0.5/monominoes.fa.js
@@ -0,0 +1,52 @@
+/**
+ * Font Awesome renders.
+ * Contained in a new package.
+ */
+Monominoes.fa = {};
+
+Monominoes.fa.ICON = Monominoes.renders.I.extend({
+  "name": "FA_ICON",
+  "icon": Monominoes.util.data, // Name from library (example: "camera-retro", without "fa"). Default is data itself.
+  "size": "", // Add size: lg, 2x, 3x, 4x, 5x.  
+  "fixed-width": false,
+  "border": false,
+  "pull": "",
+  "animated": "", // Choose between pulse and spin.
+  "rotate": "", // Choose between 90, 180 and 270.
+  "flip": "", // Choose between horizontal and vertical.
+  "color": "", // Default existent.
+  "background": "", // Default existent.
+  "buildItem": function(data,target) {
+    var cls = "";
+    var self,item,pivot;
+    var append = function(str,cls) {
+      return Komunalne.util.append(str,Monominoes.util.format("fa-{0}",cls));
+    };
+    
+    this.config.def = this.config.def || {};
+    this.config.def.style = this.config.def.style || {};
+    this.config.def.style.color = (this.color || this.config.def.style.color);
+    this.config.def.style["background-color"] = (this.background || this.config.def.style["background-color"]);
+    
+    item = this.super.buildItem(data,target);
+    self = this;
+    pivot = {};
+    Komunalne.util.forEach(["size","pull","animated","rotate","flip","icon"],function(val) {
+      pivot[val] = Monominoes.util.extractValue(self[val],self,target,data);
+    });
+    
+    cls = Komunalne.util.append("fa",item.attr("class"));
+    cls = append(cls,pivot.icon);
+    cls = Komunalne.util.isAnyOf(pivot.size,"lg","2x","3x","4x","5x") ? append(cls,pivot.size) : cls;
+    cls = Komunalne.util.isAnyOf(pivot.pull,"left","right") ? append(cls,"pull-"+pivot.pull) : cls;
+    cls = Komunalne.util.isAnyOf(pivot.animated,"pulse","spin") ? append(cls,pivot.animated) : cls;
+    cls = Komunalne.util.isAnyOf(pivot.rotate,"90","180","270") ? append(cls,"rotate-"+pivot.rotate) : cls;
+    cls = Komunalne.util.isAnyOf(pivot.flip,"horizontal","vertical") ? append(cls,"flip-"+pivot.flip) : cls;
+    
+    cls = this["fixed-width"] === true ? append(cls,"fw") : cls;
+    cls = this.border === true ? append(cls,"border") : cls;
+    item.addClass(cls);
+    
+    return item;
+  }
+});
